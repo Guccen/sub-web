3856e7104d27096f20f83683955eb79bbc3c6996
Guccen <60386727+Guccen@users.noreply.github.com>
2020-09-08 18:36:30 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -319,7 +319,7 @@ export default {
     };
   },
   created() {
-    document.title = "Subscription Converter";
+    document.title = "NPSS 订阅转换";
     this.isPC = this.$getOS().isPc;
 
     // 获取 url cache
